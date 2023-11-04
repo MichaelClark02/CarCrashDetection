dbbf784354adfe0068549deb3ab9136d3bae551c
Michael Clark <michaelpclark03@gmail.com>
2023-11-04 15:46:58 -0500

    video uploadable

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
 const app = express();
-const port = 3000; // You can change this to any port you prefer
+const port = 3000;
 
-// Define a route for the root URL
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+// Serve the static HTML file
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configure multer for video uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Save uploaded videos to the 'uploads' directory
+  },
+  filename: (req, file, cb) => {
+    const videoName = `${Date.now()}-${file.originalname}`;
+    cb(null, videoName);
+  },
+});
+const upload = multer({ storage });
+
+// Route for uploading videos
+app.post('/upload', upload.single('video'), (req, res) => {
+  res.json({ message: 'Video uploaded successfully' });
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
