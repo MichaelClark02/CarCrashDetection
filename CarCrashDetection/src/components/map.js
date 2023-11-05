00c4579ae22edd68586f900c332a1b8894bc5d1e
Michael Clark <michaelpclark03@gmail.com>
2023-11-05 02:36:54 -0600

    .

--- CarCrashDetection/src/components/map.js
+++ CarCrashDetection/src/components/map.js
@@ -7,9 +7,67 @@ import { Video, ResizeMode } from 'expo-av'
 import 'react-native-gesture-handler'
 
 
+async function fetchData(id) {
+  try {
+    const url = "127.0.0.1:8000/watch/" + id 
+    const response = await fetch(url); // Replace with the URL of the API you want to fetch data from
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json(); // Parse the response as JSON
+    console.log(data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function fetchAllData() {
+  try {
+    const url = "127.0.0.1:8000/videos/";
+    const response = await fetch(url); // Replace with the URL of the API you want to fetch data from
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json(); // Parse the response as JSON
+    console.log(data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+  return data
+}
+
+
 
 export default function Map({ navigation }) {
-  const video = React.useRef(null)
+  let currentVid;
+  const allVids = fetchAllData();
+  const jsonObject = JSON.parse(allVids);
+
+  jsonObject.forEach((item, index) => {
+    //make marker
+    //title = item.file_id
+    //longitude = item.location.lon
+    //latitude = item.location.lat
+    //extra note = item.upload_date
+    //all markers need this flag
+    //onPress={currVid = fetchData('item.file_id')}
+    //on all video tags the source = currVid
+
+    //ignore
+    // console.log(`Object ${index + 1}:`);
+    // console.log("ID:", item._id);
+    // console.log("File ID:", item.file_id);
+    // console.log("Filename:", item.filename);
+    // console.log("Location (lat):", item.location.lat);
+    // console.log("Location (lon):", item.location.lon);
+    // console.log("Upload Date:", item.upload_date);
+    // console.log("\n");
+  });
+  
+  
+  const video = React.useRef(null);
   const [lat, setLat] = useState(0);
   const [lon, setLong] = useState(0);
   const [status, setStatus] = React.useState({});
@@ -43,10 +101,12 @@ export default function Map({ navigation }) {
         }}
       >
         <Marker 
+        onPress={fetchData('asdasd')}
         coordinate={{
           latitude: 32.98626276350026,
           longitude: -96.75027303423546
         }} 
+        title=id
         //onPress={() => navigation.navigate("Video")}
         >
           <FontAwesome5 name="car-crash" size={32} color="black" style={styles.icon}/>
